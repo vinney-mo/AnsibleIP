15b30537ad07ad8926cdfcaca9db79780397c9e1
vinney-mo <vinneymotanya@gmail.com>
2023-06-15 14:56:50 +0300

    Terraform initial commit

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,18 +1,31 @@
-resource "aws_instance" "my-machine" {
-  # Creates two identical EC2 instances
-  count = 2
+resource "aws_key_pair" "my_key_pair" {
+  key_name   = "my-key-pair"
+  public_key = file("~/.ssh/id_rsa.pub")  # Path to your public key
+}
 
-  # All instances will have the same AMI and instance type
-  ami           = lookup(var.ec2_ami, var.region)
-  instance_type = "t3.micro"  # Instance type with 2 vCPUs and 1GB memory
-
-  root_block_device {
-    volume_size = 10  # 10GB disk space
-  }
+resource "aws_instance" "my_instance" {
+  ami           = var.ec2_ami  # Replace with your desired AMI ID
+  instance_type = "t3.micro"      # Replace with your desired instance type
+  key_name      = aws_key_pair.my_key_pair.key_name
 
   tags = {
-    # The count.index allows you to launch a resource
-    # starting with the distinct index number 0 and corresponding to this instance.
-    Name = "my-machine-${count.index}"
+    Name = "my-new-instance"
+  }
+}
+
+# ansible.tf
+resource "null_resource" "configure_ansible" {
+  connection {
+    type        = "ssh"
+    user        = "ec2-user"  # Replace with the appropriate username for your AMI
+    private_key = file("~/.ssh/id_rsa")  # Path to your private key
+    host        = aws_instance.my_instance.public_ip
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo sed -i '/${aws_instance.my_instance.private_ip}/d' /etc/ansible/hosts",
+      "sudo bash -c 'echo ${aws_instance.my_instance.private_ip} ${aws_instance.my_instance.tags.Name} >> /etc/ansible/hosts'",
+    ]
   }
 }
