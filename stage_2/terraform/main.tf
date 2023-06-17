c2fda65f528cfcda8a9fcf6af4cde8be135d9ab4
vinney-mo <vinneymotanya@gmail.com>
2023-06-17 18:17:29 +0300

    Refactored to update to ansible terraform integration

--- stage_2/terraform/main.tf
+++ stage_2/terraform/main.tf
@@ -1,15 +1,44 @@
-resource "aws_key_pair" "example" {
-  key_name   = "host-3-key"
+resource "aws_key_pair" "my-key" {
+  key_name   = "host-4-key"
   public_key = var.public_key  # Update the key in the.tfvars file
+}
+
+resource "aws_security_group" "security_group" {
+  name        = "My Security Group"
+  description = "Allow SSH, HTTP, and HTTP traffic"
+
+  #Allow SSH
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  #Allow HTTP
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  #Allow HTTP
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_instance" "example" {
   ami           = "ami-016b30666f212275a"  # Change this to your desired AMI ID
   instance_type = "t3.micro"  # Change this to your desired instance type
 
-  key_name      = aws_key_pair.example.key_name
+  key_name      = aws_key_pair.my-key.key_name
 
-  vpc_security_group_ids = ["sg-09e9f35f02c3890e1"] # Change this to your desired group ID. Default to allow ssh
+  vpc_security_group_ids = aws_security_group.security_group
 
   tags = {
     Name = "Ansible-Host-3"
@@ -17,7 +46,7 @@ resource "aws_instance" "example" {
 
   provisioner "local-exec" {
     command = <<-EOT
-      echo "host_3 ansible_host=${aws_instance.example.public_dns}" >> /etc/ansible/hosts
+      echo "host_4 ansible_host=${aws_instance.example.public_dns}" >> /etc/ansible/hosts
     EOT
   }
 
